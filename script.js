da660dce30b2c1852c7b50b35ef317f5024f0779
Ronie2022 <timbalroniejohn@gmail.com>
2023-02-11 13:32:04 +0800

    modify api linl

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
 
-  const response = await fetch("https://chatbot-7oed.onrender.com", {
+  const response = await fetch("https://chatbot-df65.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
